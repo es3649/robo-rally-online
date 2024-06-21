088c9ad14055ebed18393bf2dab106bb67689183
Steadman, Eric <eric.steadman@intel.com>
2024-06-21 08:13:29 -0700

    adds other init files

---
+++ src/components/GameSettings.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+// access the store. This will be important for saving the game settings
+</script>
+
+<template>
+    <input name="lobby_code" type="text" placeholder="lobby code">
+</template>
---
+++ src/views/SettingsView.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import GameSettings from '../components/GameSettings.vue'
+</script>
+
+<template>
+    <main>
+        <GameSettings />
+    </main>
+</template>
