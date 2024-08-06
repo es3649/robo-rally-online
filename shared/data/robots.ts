0fbba6bebd741d8f6c5356ad896bbd92292c54e9
Eric Steadman <es3649@gmail.com>
2024-08-06 16:07:56 -0700

    added robot data

--- shared/data/robots.ts
+++ shared/data/robots.ts
@@ -1,4 +1,4 @@
-import { Character } from './models/player'
+import { Character } from '../models/player'
 
 export namespace Robots {
     const Twonky: Character = {
@@ -7,8 +7,32 @@ export namespace Robots {
         sprite_large: ""
     }
 
-    const HulkX90: Character = {
-        name: "Hulk-X90",
+    const PanzerX90: Character = {
+        name: "Panzer-X90",
+        sprite_small: "",
+        sprite_large: ""
+    }
+
+    const Bladestorm: Character = {
+        name: "Bladestorm",
+        sprite_small: "",
+        sprite_large: ""
+    }
+
+    const ZephyrM2: Character = {
+        name: "Zephyr (Mark II)",
+        sprite_small: "",
+        sprite_large: ""
+    }
+
+    const AxelV8: Character = {
+        name: "Axel-V8",
+        sprite_small: "",
+        sprite_large: ""
+    }
+
+    const Thor: Character = {
+        name: "Thor",
         sprite_small: "",
         sprite_large: ""
     }
